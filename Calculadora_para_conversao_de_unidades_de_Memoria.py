0088fdf1462234e5a764d05ae86473410f6fcb4a
unknown <Matheus.SantosTSIS@gmail.com>
2023-03-23 14:06:51 -0300

    Calculadora criada, conversao de Bits pra byte e vice-versa

---
+++ Calculadora_para_conversao_de_unidades_de_Memoria.py
@@ -0,0 +1,21 @@
+
+#Bit para Byte
+
+
+def converterStringParaFloat(value):
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    bytesCalculado = valorASerConvertido / 8
+    return bytesCalculado
+
+def byteParaBit(valorASerConvertido):
+    print('Valor convertido de byte para bit')
+    bitsCalculado = valorASerConvertido * 8
+    return bitsCalculado
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = byteParaBit(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
